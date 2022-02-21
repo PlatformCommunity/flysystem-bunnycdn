38dc38554507bc2ac61059443ea4d0c7989feafd
Alex <2058557+sifex@users.noreply.github.com>
2022-02-21 14:40:08 +0000

    Nah I actually can support 7.2

--- src/BunnyCDNAdapter.php
+++ src/BunnyCDNAdapter.php
@@ -142,7 +142,7 @@ class BunnyCDNAdapter implements FilesystemAdapter
                 ),
                 $bunny_file_array['Length'],
                 Visibility::PUBLIC,
-                date_create_from_format('Y-m-d\TH:i:s.v', $bunny_file_array['LastChanged'])->getTimestamp(),
+                date_create_from_format('Y-m-d\TH:i:s.u', $bunny_file_array['LastChanged'].'000')->getTimestamp(),
                 $bunny_file_array['ContentType'],
                 $this->extractExtraMetadata($bunny_file_array)
             );
@@ -160,7 +160,7 @@ class BunnyCDNAdapter implements FilesystemAdapter
             'dirname'   => Util::splitPathIntoDirectoryAndFile($bunny_file_array['Path'])['dir'],
             'guid' => $bunny_file_array['Guid'],
             'object_name' => $bunny_file_array['ObjectName'],
-            'timestamp' => date_create_from_format('Y-m-d\TH:i:s.v', $bunny_file_array['LastChanged'])->getTimestamp(),
+            'timestamp' => date_create_from_format('Y-m-d\TH:i:s.u', $bunny_file_array['LastChanged'].'000')->getTimestamp(),
             'server_id' => $bunny_file_array['ServerId'],
             'user_id' => $bunny_file_array['UserId'],
             'date_created' => $bunny_file_array['DateCreated'],
